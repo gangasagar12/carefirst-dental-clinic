c9d9cb744c672bca74d2477b6e348bd88180c106
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-08-18 20:51:00 +0545

    fix(dashboard): finalize single dashboard namespace URL routing

--- core/urls.py
+++ core/urls.py
@@ -46,7 +46,6 @@ urlpatterns += i18n_patterns(
     path('appointment/', include('appointments.urls')),
     path('blog/', include('blogs.urls')),
     path('videos/', include('media_center.urls')),
-    path('dashboard/', include('dashboard.urls')),
     path('admin/dashboard/', inquiries_dashboard, name='admin_dashboard'),
     path('admin/password_reset/', custom_auth_views.OTPRequestView.as_view(), name='admin_password_reset'),
     path('admin/password_reset/done/', custom_auth_views.OTPVerifyView.as_view(), name='password_reset_done'),
